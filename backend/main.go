ae1714e29eedaa4d7804b7d3451321984fb2a1d4
Hashim Hashimov <hashimov99@mail.ru>
2021-03-05 15:08:23 +0200

    Level field has been added

--- backend/main.go
+++ backend/main.go
@@ -2,15 +2,17 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
 	_ "github.com/mattn/go-sqlite3"
 )
 
 type Tasks struct {
-	Id     int    `gorm:"AUTO_INCREMENT" form:"id" json:"id"`
+	ID     int    `gorm:"AUTO_INCREMENT" form:"id" json:"id"`
 	Title  string `gorm:"not null" form:"title" json:"title"`
 	Status bool   `gorm:"not null" form:"status" json:"status"`
+	Level  int    `gorm: "default:1" form:"level" json:"level"`
 }
 
 // DB Functions
@@ -73,7 +75,7 @@ func GetTask(c *gin.Context) {
 	// SELECT * FROM users WHERE id = 1;
 	db.First(&task, id)
 
-	if task.Id != 0 {
+	if task.ID != 0 {
 		// Display JSON result
 		c.JSON(200, task)
 	} else {
@@ -96,14 +98,15 @@ func UpdateTask(c *gin.Context) {
 
 	if task.Title != "" {
 
-		if task.Id != 0 {
+		if task.ID != 0 {
 			var newTask Tasks
 			c.Bind(&newTask)
 
 			result := Tasks{
-				Id:     task.Id,
+				ID:     task.ID,
 				Title:  newTask.Title,
 				Status: newTask.Status,
+				Level:  newTask.Level,
 			}
 
 			db.Save(&result)
@@ -121,25 +124,25 @@ func UpdateTask(c *gin.Context) {
 
 func DeleteTask(c *gin.Context) {
 	// Connection to the database
-    db := InitDb()
-    // Close connection database
-    defer db.Close()
+	db := InitDb()
+	// Close connection database
+	defer db.Close()
 
-    // Get id user
-    id := c.Params.ByName("id")
-    var task Tasks
-    // SELECT * FROM users WHERE id = 1;
-    db.First(&task, id)
+	// Get id user
+	id := c.Params.ByName("id")
+	var task Tasks
+	// SELECT * FROM users WHERE id = 1;
+	db.First(&task, id)
 
-    if task.Id != 0 {
-        // DELETE FROM users WHERE id = user.Id
-        db.Delete(&task)
-        // Display JSON result
-        c.JSON(200, gin.H{"success": "Task #" + id + " deleted"})
-    } else {
-        // Display JSON error
-        c.JSON(404, gin.H{"error": "Task not found"})
-    }
+	if task.ID != 0 {
+		// DELETE FROM users WHERE id = user.Id
+		db.Delete(&task)
+		// Display JSON result
+		c.JSON(200, gin.H{"success": "Task #" + id + " deleted"})
+	} else {
+		// Display JSON error
+		c.JSON(404, gin.H{"error": "Task not found"})
+	}
 }
 
 // DB FUnctions
